321734d152e50eb003630a0a8b0aa66ffcb6d19a
Olena Pohybil <itlentus@gmail.com>
2022-05-08 23:48:20 +0300

    fix submit event

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,11 +1,13 @@
 import throttle from "lodash.throttle"
 
 const STORAGE_KEY = "feedback-form-state"
+
 const form = document.querySelector(".feedback-form");
 const email = document.querySelector("input");
 const textarea = document.querySelector("textarea");
 
-const formData = JSON.parse(localStorage.getItem(STORAGE_KEY))||{};
+const formData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
+
 if (formData.email) { email.value = formData.email };
 if (formData.message) { textarea.value = formData.message };
 
@@ -17,15 +19,23 @@ const setData = (e) => {
 }
 const onSubmitForm = (e) => {
 
-    // e.preventDefault() 
-    // убрала, так как если страница не перезагружается при отправке, 
-    // то в дальнейшем в локальное хранилище при заполнении одного поля и последующей 
-    // перезагрузке подтягивается предыдущее значение второго поля и оно заполняется в форму
-    
+    e.preventDefault() 
+
+    if (!formData.email || !formData.message) {   
+        
+        alert("Заповніть, будь ласка, всі поля форми")       
+    }
+
+    else {
+
     console.log(formData)
 
     e.currentTarget.reset();
     localStorage.removeItem(STORAGE_KEY);
+
+    formData.email = '';
+    formData.message = '';
+    }
 }
 
 form.addEventListener('input', throttle((setData), 500));
