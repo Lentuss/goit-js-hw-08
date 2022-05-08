3144f92d7d628ef643d0f739fd5d65f944057eef
Olena Pohybil <itlentus@gmail.com>
2022-05-08 22:07:40 +0300

    task 3

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Change code below this line
+const gallery = document.querySelector(".gallery");
+
+const createGallery = () => {
+    galleryItems.map(({ preview, original, description }) => {
+
+    const galleryItem = `<div class="gallery__unit">
+  <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+</div>`
+        
+gallery.insertAdjacentHTML('afterbegin', galleryItem);
+    })
+}
+
+createGallery();
 
 console.log(galleryItems);
+
+let galleryLightbox = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionsData: 'alt',
+    captionDelay: 250
+})
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,33 @@
+import throttle from "lodash.throttle"
 
+const STORAGE_KEY = "feedback-form-state"
+const form = document.querySelector(".feedback-form");
+const email = document.querySelector("input");
+const textarea = document.querySelector("textarea");
+
+const formData = JSON.parse(localStorage.getItem(STORAGE_KEY))||{};
+if (formData.email) { email.value = formData.email };
+if (formData.message) { textarea.value = formData.message };
+
+const setData = (e) => {
+
+    formData[e.target.name] = e.target.value;
+
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData))
+}
+const onSubmitForm = (e) => {
+
+    // e.preventDefault() 
+    // убрала, так как если страница не перезагружается при отправке, 
+    // то в дальнейшем в локальное хранилище при заполнении одного поля и последующей 
+    // перезагрузке подтягивается предыдущее значение второго поля и оно заполняется в форму
+    
+    console.log(formData)
+
+    e.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+}
+
+form.addEventListener('input', throttle((setData), 500));
+
+form.addEventListener('submit', onSubmitForm)
